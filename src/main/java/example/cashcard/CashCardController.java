1e85e0d223cc7eff827601289997e67757f4a40b
JohnySapo <B00139596@mytudublin.ie>
2024-05-14 21:21:54 +0100

    Implement Spring Boot POST method for this lab.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,11 +1,16 @@
 package example.cashcard;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.Optional;
 
 @RestController
@@ -36,5 +41,14 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
+
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -13,6 +13,11 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URI;
+
+import javax.swing.text.Document;
 
 // import java.io.IOException;
 
@@ -49,24 +54,44 @@ class CashCardJsonTest {
 	// assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
 	// }
 
-	@Test
-	void shouldReturnACashCardWhenDataIsSaved() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+	// @Test
+	// void shouldReturnACashCardWhenDataIsSaved() {
+	// ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99",
+	// String.class);
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		DocumentContext documentContext = JsonPath.parse(response.getBody());
+	// assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	// DocumentContext documentContext = JsonPath.parse(response.getBody());
+	// Number id = documentContext.read("$.id");
+	// assertThat(id).isEqualTo(99);
+
+	// Double amount = documentContext.read("$.amount");
+	// assertThat(amount).isEqualTo(123.45);
+	// }
+
+	// @Test
+	// void shouldNotReturnACashCardWithAnUnknownId() {
+	// ResponseEntity<String> response =
+	// restTemplate.getForEntity("/cashcards/1000", String.class);
+
+	// assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	// assertThat(response.getBody()).isBlank();
+	// }
+
+	@Test
+	void shouldCreateANewCashCard() {
+		CashCard newCashCard = new CashCard(44L, 250.00);
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
 		Number id = documentContext.read("$.id");
-		assertThat(id).isEqualTo(99);
-
 		Double amount = documentContext.read("$.amount");
-		assertThat(amount).isEqualTo(123.45);
-	}
 
-	@Test
-	void shouldNotReturnACashCardWithAnUnknownId() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
-
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-		assertThat(response.getBody()).isBlank();
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.00);
 	}
 }
