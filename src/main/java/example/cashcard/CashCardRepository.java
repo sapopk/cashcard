9d073dd589065314ccd75731964b8a8905c05be0
JohnySapo <B00139596@mytudublin.ie>
2024-05-14 21:21:54 +0100

    Implement Spring Boot POST method for this lab.

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,8 +1,5 @@
 package example.cashcard;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.CrudRepository;
 
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
-    record CashCard(@Id Long id, Double amount) {}
-}
+interface CashCardRepository extends CrudRepository<CashCard, Long> {}
