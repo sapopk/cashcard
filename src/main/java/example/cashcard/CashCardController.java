673c306381aa6054575c62f68678d14f4260f814
JohnySapo <B00139596@mytudublin.ie>
2024-05-17 22:45:43 +0100

    Implement the lab of class Return a list of GET HTTP Method.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,6 +1,10 @@
 package example.cashcard;
 
 import org.apache.catalina.connector.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +15,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,16 +28,6 @@ public class CashCardController {
     private CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }
-
-    // @GetMapping("/{requestedId}")
-    // private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-    //     if (requestedId.equals(99L)) {
-    //         CashCard cashCard = new CashCard(99L, 123.45);
-    //         return ResponseEntity.ok(cashCard);
-    //     } else {
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
@@ -50,5 +46,16 @@ public class CashCardController {
         URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
 
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+        ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
